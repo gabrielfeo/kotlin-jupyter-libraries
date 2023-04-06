d40018d6ac4f4e7cd18ff45a71cdc4654a50c276
Gabriel Feo <gabriel@gabrielfeo.com>
2023-04-06 23:15:14 +0100

    Revert tests

--- .github/workflows/renovate-config.js
+++ .github/workflows/renovate-config.js
@@ -1,10 +1,3 @@
-// const fs = require('fs');
-// const configFilePath = "P:\\projects\\workspace\\net.twisterrob.gradle\\.github\\renovate.json";
-// const configFileText = fs.readFileSync(configFilePath, "utf8");
 module.exports = {
-	"dryRun": "extract",
-	"logFile": "/tmp/renovate.log",
-	"logFileLevel": "info",
 	"repositories": ["gabrielfeo/kotlin-jupyter-libraries"],
-//	"logFormat": "text",
 };
