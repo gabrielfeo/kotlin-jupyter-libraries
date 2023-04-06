3ee2f72a84a8b36802492bf08cccc8107742bed9
Gabriel Feo <gabriel@gabrielfeo.com>
2023-04-06 22:50:13 +0100

    Test

---
+++ .github/workflows/renovate-config.js
@@ -0,0 +1,10 @@
+// const fs = require('fs');
+// const configFilePath = "P:\\projects\\workspace\\net.twisterrob.gradle\\.github\\renovate.json";
+// const configFileText = fs.readFileSync(configFilePath, "utf8");
+module.exports = {
+	"dryRun": "extract",
+	"logFile": "renovate.log",
+	"logFileLevel": "info",
+	"repositories": ["gabrielfeo/kotlin-jupyter-libraries"],
+//	"logFormat": "text",
+};
