3c24aaae1e577fc3bb817e0c07769d2447a93441
Gabriel Feo <gabriel@gabrielfeo.com>
2023-04-11 20:47:43 +0100

    Fix and improve logging

--- .github/scripts/check_renovate_matches.py
+++ .github/scripts/check_renovate_matches.py
@@ -27,27 +27,30 @@ def main(argv=None):
     args = parser.parse_args(argv)
     print("Parsing dependencies of library descriptors...", file=sys.stderr)
     packages = parse_descriptor_dependencies(args.descriptors_dir)
-    print("Checking packages matched by Renovate in '{args.log}'...", file=sys.stderr)
+    print(f"Checking packages matched by Renovate in '{args.log}'...", file=sys.stderr)
     unmatched = get_packages_not_in_log(packages, args.log)
     if unmatched:
         print("Unmatched packages:", file=sys.stderr)
         print('\n'.join(unmatched))
         exit(1)
+    else:
+        print("All packages were matched")
 
 
 def parse_descriptor_dependencies(descriptors_dir: Path):
     packages = set()
-    for descriptor in get_library_descriptors(descriptors_dir):
+    for path, descriptor in get_library_descriptors(descriptors_dir):
         for dependency in extract_dependencies(descriptor):
             package = dependency_to_package_name(dependency)
             packages.add(package)
-            print(f"[{descriptor}] {package}", file=sys.stderr)
+            print(f"[{path}] {package}", file=sys.stderr)
     return packages
 
 
 def get_library_descriptors(dir: Path):
     """Get library descriptors in JSON format"""
-    return (json.loads(p.read_text()) for p in dir.glob('*.json'))
+    for path in dir.glob('*.json'):
+        yield path, json.loads(path.read_text())
 
 
 def extract_dependencies(descriptor: dict):
