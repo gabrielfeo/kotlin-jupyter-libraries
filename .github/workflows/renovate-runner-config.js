e0e3cd3d8ad8aa1b7b313a66b79eb6eac7967e83
Gabriel Feo <gabriel@gabrielfeo.com>
2023-04-30 04:22:25 +0100

    Prevent GitHub API rate-limiting

--- .github/workflows/renovate-runner-config.js
+++ .github/workflows/renovate-runner-config.js
@@ -1,3 +1,9 @@
 module.exports = {
 	"repositories": ["gabrielfeo/kotlin-jupyter-libraries"],
+	"hostRules": [
+    {
+      "matchHost": "api.github.com",
+      "concurrentRequestLimit": 2
+    }
+  ]
 };
