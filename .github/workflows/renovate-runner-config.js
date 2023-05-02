51e6e535748b2bbb9e0eba27f5bebf28acb842bb
Gabriel Feo <gabriel@gabrielfeo.com>
2023-05-02 03:31:41 +0100

    Replace config.js

--- .github/workflows/renovate-runner-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	"repositories": ["gabrielfeo/kotlin-jupyter-libraries"],
-	"hostRules": [
-    {
-      "matchHost": "api.github.com",
-      "concurrentRequestLimit": 2
-    }
-  ]
-};
